a68ba61ffc41532980506878e2b0339ce2410309
Jorio <jjrzx@outlook.com>
2024-02-17 04:07:39 -0500

    stem player base

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,21 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      width: {
+        '128': '32rem',
+      },
+      height: {
+        '128': '32rem',
+      },
+      boxShadow: {
+        'stem-shadow': 'inset -23px -39px 44px 7px rgba(0,0,0,0.2),0px 10px 12px -5px rgba(0,0,0,0.5),inset 7px 10px 27px -3px rgba(255,255,255,0.3)',  
+        'stem-inner-button' : 'inset 0px -3px 5px 0px rgba(0,0,0,0.05),inset 0px 5px 5px -5px rgba(255,255,255,0.3)',
+        'stem-inner-mid' : 'inset 0px 0px 9px 0px rgba(0,0,0,0.2),0px -3px 10px 0px rgba(0,0,0,0.1)',
+        'stem-inner-top' : 'inset 0px 10px 21px 0px rgba(0,0,0,0.1),inset 0px -14px 13px -14px rgba(255,255,255,0.3),0px -17px 52px -12px rgba(0,0,0,0.1)',
+        'stem-inner-bottom' : 'inset 0px 10px 21px 0px rgba(0,0,0,0.1),inset 0px -14px 13px -14px rgba(255,255,255,0),0px -17px 52px -12px rgba(0,0,0,0.1)',
+        'stem-inner-left' : 'inset -5px 15px 13px 0px rgba(0,0,0,0.1),inset 0px -10px 20px -5px rgba(255,255,255,0.15),0px -3px 10px 0px rgba(0,0,0,0.1)',
+        'stem-inner-right' : 'inset 5px 15px 13px 0px rgba(0,0,0,0.1),inset 0px -10px 20px -5px rgba(255,255,255,0.15),0px -3px 10px 0px rgba(0,0,0,0.1)',
+      },
     },
   },
   plugins: [],
